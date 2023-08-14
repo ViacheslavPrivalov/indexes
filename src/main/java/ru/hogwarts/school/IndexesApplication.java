63e29b79a07463544205be1d389eb58ee6dd9a16
Viacheslav Privalov <privalov2306@yandex.ru>
2023-08-14 14:52:58 +0300

    Init commit

---
+++ src/main/java/ru/hogwarts/school/IndexesApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndexesApplication.class, args);
+    }
+
+}
